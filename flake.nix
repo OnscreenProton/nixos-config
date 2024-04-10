2e640380c656622e1c872f03143d33b02585f4f1
OnscreenProton <onscreenproton@gmail.com>
2024-04-09 21:19:15 -0700

    Accidently deleted vscode-server a while ago

--- flake.nix
+++ flake.nix
@@ -37,6 +37,11 @@
       url = "github:nix-community/NixOS-WSL";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    vscode-server = {
+      url = "github:nix-community/nixos-vscode-server";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
